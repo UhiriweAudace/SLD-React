b8440d5a88810be86b6111d4f5d0cc5685201f98
UhiriweAudace <u.audace@gmail.com>
2020-09-16 21:24:01 +0200

    slides issue fixed

---
+++ src/components/Slides.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+function Slides({ slides }) {
+    const [initial, setInitial] = useState(1)
+    const [disabled, setDisabled] = useState(true);
+    const [prevDisable, setPrevDisable] = useState(true)
+    const [nextDisable, setNexDisable] = useState(false)
+
+    const onRestartHandler = () => {
+        setInitial(0)
+        setDisabled(true)
+        setPrevDisable(true)
+        setNexDisable(false)
+    }
+    const onPrevHandler = () => {
+        if (initial === 0) {
+            setDisabled(true)
+            setPrevDisable(true)
+            setNexDisable(false)
+        } else {
+            setInitial(initial - 1)
+        }
+    }
+    const onNextHandler = () => {
+        console.log(slides.length, initial)
+        setInitial(initial + 1)
+        if (slides.length - 2 === initial) {
+            setNexDisable(true)
+        }
+        setInitial(initial + 1)
+        setDisabled(false)
+        setPrevDisable(false)
+    }
+
+    return (
+        <div>
+            <div id="navigation" className="text-center">
+                <button data-testid="button-restart" className="small outlined" onClick={() => onRestartHandler()} disabled={disabled}>Restart</button>
+                <button data-testid="button-prev" className="small" onClick={() => onPrevHandler()} disabled={prevDisable}>Prev</button>
+                <button data-testid="button-next" className="small" onClick={() => onNextHandler()} disabled={nextDisable}>Next</button>
+            </div>
+
+            {
+                slides.map((slide, index) => {
+                    return initial === index && (
+                        <div id="slide" className="card text-center" key={index}>
+                            <h1 data-testid="title">{slide.title}</h1>
+                            <p data-testid="text">{slide.text}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+
+}
+
+export default Slides;
